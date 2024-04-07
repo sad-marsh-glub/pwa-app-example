e3267d51c62b75e8c993173abf5104d8e976d790
sad-marsh-glub <151405638+sad-marsh-glub@users.noreply.github.com>
2024-04-06 18:32:27 -0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,45 @@
+var CACHE_NAME = 'hi';
+var offlineFundamentals = [
+  '',
+  'app.js',
+  '
+]; 
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(offlineFundamentals);
+      })
+  );
+});
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['example'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
